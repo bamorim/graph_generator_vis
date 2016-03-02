56a65ce30677120e14a5af2b2b054f91d84a373d
Bernardo Amorim <contato@bamorim.com>
2016-03-02 00:07:38 -0300

    Completely changed user interface

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-var svg = d3.select("body").append("svg")
-.attr("width", window.innerWidth)
-.attr("height", window.innerHeight);
+alertify.defaults.transition = 'zoom';
 
+// Monkey paths
 Array.prototype.replace = function replace(source){
   this.splice.apply(this,[0,this.length].concat(source))
 }
 
-var color = d3.scale.category10();
+// Graph logic
 
 function Graph(_directed){
   thisdirected = !!_directed;
@@ -33,6 +32,58 @@ Graph.prototype.reset = function(){
   this.edgeList = [];
 }
 
+// Initialization
+
+var Config = {
+  autozoom: false,
+  frequencies: [1,2,5,10,20,50,100,200,500]
+};
+
+var svg = d3.select("body").append("svg")
+.attr("width", window.innerWidth)
+.attr("height", window.innerHeight);
+
+// Zoom Behaviour
+
+var zoom = d3.behavior.zoom().on('zoom',zoomed);
+svg.call(zoom);
+
+function autozoom(){
+  if(nodes.length == 0) return;
+  var centroid = nodes.reduce(function(a,b){return {x: a.x+b.x, y: a.y+b.y};},{x: 0, y: 0});
+  var scene = nodes.reduce(function(acc,node){
+    return {
+      minX: acc.minX < node.x ? acc.minX : node.x,
+      minY: acc.minY < node.y ? acc.minY : node.y,
+      maxX: acc.maxX > node.x ? acc.maxX : node.x,
+      maxY: acc.maxY > node.y ? acc.maxY : node.y
+    };
+  },{ minX: Infinity, maxX: -Infinity, minY: Infinity, maxY: -Infinity })
+  centroid.x /= nodes.length;
+  centroid.y /= nodes.length;
+
+  var svgSize = { width: svg.attr('width'), height: svg.attr('height') };
+  var newSize = { width: scene.maxX - scene.minX, height: scene.maxY - scene.minY };
+  var newScale = svgSize.width/svgSize.height > newSize.width/newSize.height ? svgSize.height/newSize.height : svgSize.width/newSize.width;
+  newScale /= 1.1; // add a margin
+  var oldScale = zoom.scale
+
+  var scale = newScale < zoom.scale() ? newScale : zoom.scale();
+
+  var translation = [(centroid.x-svgSize.width*scale/2), (centroid.y-svgSize.height*scale/2)]
+
+  zoom.scale(scale);
+  zoom.translate(translation);
+  zoomed();
+}
+
+function zoomed(){
+  container.attr("transform", "translate(" + zoom.translate() + ")scale(" + zoom.scale() + ")");
+  update();
+}
+
+// Scene initialization
+
 var graph = new Graph();
 
 var nodes = [],
@@ -46,9 +97,12 @@ var force = d3.layout.force()
 .size([+svg.attr("width"), +svg.attr("height")])
 .on("tick", tick);
 
+var container = svg.append("g");
 
-var node = svg.selectAll(".node"),
-  link = svg.selectAll(".link");
+var node = container.selectAll(".node"),
+  link = container.selectAll(".link");
+
+var drag = force.drag().on("dragstart",function(){ d3.event.sourceEvent.stopPropagation(); });
 
 function update() {
   link = link.data(force.links());
@@ -58,12 +112,7 @@ function update() {
   node = node.data(force.nodes());
   node.enter().append("circle").attr("class","node").attr("r", 5);
   node.exit().remove();
-  node.call(force.drag);
-
-  /*node.filter(function(d){ return d.index == 0; }).append("path")
-    .attr("d",function(d){
-    return "M " + dx + " " + dy + " A 3 3 0 0 1 0 1";
-    })*/
+  node.call(drag);
 
   force.start();
 }
@@ -96,8 +145,11 @@ function tick() {
     var lineTo = ["L", d.target.x, d.target.y].join(" ");
     return [moveTo, lineTo].join(" ");
   })
+  if(Config.autozoom)
+    autozoom();
 }
 
+// Graph abstraction to both graph and svg
 function addEdge(from, to){
   graph.addEdge(from, to);
   links.push({source: from, target: to});
@@ -118,9 +170,37 @@ function reset(){
   update();
 }
 
-var operations = [];
-var interval;
-var period = 500;
+// Scheduler with varying frequency
+var Scheduler = {
+  operations: [],
+  paused: true,
+  push: function(op){ this.operations.push(op) },
+  frequency: 2,
+  start: function(){
+    this.pause();
+    this.resume();
+  },
+  resume: function(){
+    this.paused = false;
+    this.cycle();
+  },
+  cycle: function(){
+    this.timeout = window.setTimeout(this.cycle.bind(this), 1000/this.frequency);
+
+    if(this.operations.length > 0)
+      this.operations.shift()();
+  },
+  pause: function(){
+    this.paused = true;
+    window.clearTimeout(this.timeout);
+  },
+  stop: function(){
+    this.pause();
+    this.operations = [];
+  }
+}
+
+// Model implementation
 var walkerPos;
 
 function step(){
@@ -131,44 +211,30 @@ function step(){
 
 function cycle(howManyStepsFn){
   return function(){
-    for (var i = 0; i < howManyStepsFn(); i++) operations.push(step);
-    operations.push(function(){
+    for (var i = 0; i < howManyStepsFn(); i++) Scheduler.push(step);
+    Scheduler.push(function(){
       addEdge(walkerPos,addNode());
       update();
     });
-    operations.push(cycle(howManyStepsFn));
+    Scheduler.push(cycle(howManyStepsFn));
   }
 }
 
 function run(howManyStepsFn){
-  stop();
+  console.log("Starting");
+  Scheduler.stop();
+
   reset();
   walkerPos = 0;
-  operations = [cycle(howManyStepsFn)];
   addNode();
   addEdge(0,0); // Add the self loop
   update();
 
-  interval = window.setInterval(function(){
-    if(operations.length > 0)
-      operations.shift()();
-  }, period);
+  Scheduler.push(cycle(howManyStepsFn));
+  Scheduler.start();
 }
 
-function stop(){
-  window.clearInterval(interval);
-}
-
-
-function updateDimensions(){
-  var width = window.innerWidth,
-    height = window.innerHeight;
-
-  svg.attr("width", width).attr("height", height);
-  force.size([width, height]);
-  force.start();
-}
-
+// Factory for returning how many steps each time, deterministically or stochastically
 function pSteps(p){
   return function(){
     if(Math.random() > p) {
@@ -182,40 +248,110 @@ function sSteps(s){
   return function(){ return s };
 }
 
+// User Interface 
+
+var instructions = document.getElementById("instructions");
+var instructionTimeout;
+function hideInstructions(){
+  instructions.style.opacity = '0';
+  window.clearTimeout(instructionTimeout);
+  instructionTimeout = window.setTimeout(function(){
+    instructions.style.display = 'none'
+  },250);
+}
+
+function showInstructions(){
+  window.clearTimeout(instructionTimeout);
+  instructions.style.display = 'block'
+  instructions.style.opacity = '0';
+  window.setTimeout(function(){
+    instructions.style.opacity = '1';
+  },10);
+};
+
+function toggleInstructions(){
+  if(instructions.style.opacity == '0'){
+    showInstructions()
+  } else {
+    hideInstructions();
+  }
+}
+
+function onKeyPress(e){
+  switch(e.keyCode){
+    case 97: // a
+      Config.autozoom = !Config.autozoom;
+      alertify.notify('Autozoom is now set to ' + Config.autozoom);
+      break;
+    case 115: // s
+      alertify.prompt("Start a stochastic session with p=", "", function(evt, p){
+        p = parseFloat(p);
+
+        if(isNaN(p)) return alertify.error("Invalid p value");
+
+        hideInstructions();
+        run(pSteps(p));
+      });
+      break;
+    case 100: // d
+      alertify.prompt("Start a deterministic session with s=", "", function(evt, s){
+        s = parseInt(s);
+
+        if(isNaN(s)) return alertify.error("Invalid s value");
+
+        hideInstructions();
+        run(sSteps(s));
+      });
+      break;
+    case 112: // p
+      if (Scheduler.paused) {
+        alertify.success('Resumed');
+        Scheduler.resume();
+      } else {
+        alertify.notify('Paused');
+        Scheduler.pause();
+      }
+      break;
+    case 122: // z
+      autozoom();
+      break;
+    case 63: // ?
+      toggleInstructions();
+      break;
+  }
+}
+
+function onKeyDown(e){
+  switch(e.keyCode){
+    case 37: // left
+      var nextFreq = Config.frequencies.concat([]).reverse().find(function(freq){ return freq < Scheduler.frequency; });
+      if(nextFreq){
+        Scheduler.frequency = nextFreq;
+        alertify.notify("Frequency is now set at "+nextFreq);
+      }
+      break;
+    case 39: // Right
+      var nextFreq = Config.frequencies.find(function(freq){ return freq > Scheduler.frequency; });
+      if(nextFreq){
+        Scheduler.frequency = nextFreq;
+        alertify.notify("Frequency is now set at "+nextFreq);
+      }
+      break;
+  }
+}
+
+document.addEventListener("keypress",onKeyPress);
+document.addEventListener("keydown",onKeyDown);
+
+// Additional events
+
 window.onresize = updateDimensions;
 
-var pauseBtn = document.getElementById("pause-btn");
+function updateDimensions(){
+  var width = window.innerWidth,
+    height = window.innerHeight;
 
-var showFormBtn = document.getElementById("show-form-btn");
-showFormBtn.onclick = function(e){
-  e.preventDefault();
-  generatorForm.style.display = window.getComputedStyle(generatorForm).display == "none" ? "block" : "none";
-}
-var generatorForm = document.getElementById("generator-form");
-generatorForm.onsubmit = function(e){
-  e.preventDefault();
-  var step_type = e.target.elements.step_type.value;
-  var step_value = e.target.elements.step_value.value;
-
-  var val = parseFloat(step_value);
-  if(isNaN(val)){
-    alert("Invalid value");
-    return;
-  }
-
-  if(step_type == "deterministic"){
-    run(sSteps(Math.floor(val)));
-  } else {
-    run(pSteps(val));
-  }
-
-  e.target.elements.step_value.value = val;
-  generatorForm.style.display = "none";
-  pauseBtn.style.display = "block";
-}
-
-pauseBtn.onclick = function(e){
-  e.preventDefault();
-  stop();
-  pauseBtn.style.display = "none";
+  svg.attr("width", width).attr("height", height);
+  force.size([width, height]);
+  force.start();
 }
